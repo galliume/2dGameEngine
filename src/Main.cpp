c51a4fd0854af5b0b0da9d6f64dc00d0b4139156
gcconantc <gcconantc@gmail.com>
2020-01-06 23:18:42 +0100

    Game Loop

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,19 @@
-#include <iostream>
-using namespace std;
+#include "./Constants.h"
+#include "./Game.h";
 
 int main(int argc, char *args[]) {
-    cout << "Game is running" << endl;
+    
+    Game *game = new Game();
+
+    game->Initialize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    while (game->IsRunning()) {
+        game->ProcessInput();
+        game->Update();
+        game->Render();
+    }
+
+    game->Destroy();
 
     return 0;
 }
